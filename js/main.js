bc37b3b62e15284db32c02a48adb5ff3e2ebc940
Simone <simone.pagotto.kg@gmail.com>
2020-11-20 16:39:16 +0100

    css, vue
    inseriti data e completato layout grezzo

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const myBoolzApp = new Vue ({
+  el:'#root',
+  data:{
+    userContact: {
+      name: 'Simone Pagotto',
+      avatar: 'img/avatar_io.jpg',
+    },
+    contacts: [
+      {
+        name: 'Marino Lamantino',
+        avatar: 'img/avatar_1.jpg',
+      },
+      {
+        name: 'Angelo Taggiasca',
+        avatar: 'img/avatar_2.jpg',
+      },
+      {
+        name: 'Paolo Lo Squalo',
+        avatar: 'img/avatar_3.jpg',
+      }
+    ]
+
+
+}
+
+})
